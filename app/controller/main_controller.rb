347010078d33d3d38013806295ac98891d480b7d
Abdul Khan <akhan17@outlook.com>
2020-06-03 14:01:11 -0500

    set up connection

---
+++ app/controller/main_controller.rb
@@ -0,0 +1,9 @@
+class MainController < Sinatra::Base
+    register Sinatra::ActiveRecordExtension
+    set :session_secret, "my_application_secret"
+    set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/' do
+        erb :'main_page'
+    end
+end 
