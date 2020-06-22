eceab923d4b3ea8672970b2cf2a8e8a14b2a4f64
Abdul Khan <akhan17@outlook.com>
2020-06-22 14:38:18 -0500

    updated routes

--- app/controller/main_controller.rb
+++ app/controller/main_controller.rb
@@ -42,11 +42,11 @@ class MainController < Sinatra::Base
         
     end
 
-    post '/registration' do
+    post '/signup' do
             @user = Employee.create(name: params[:name], username: params[:username], password: params[:password])
             @user[:sesh_id] = rand(1..100000000000)
             @user.save
-            redirect "/dynamic/#{@user.id}/#{@user[:sesh_id]}/welcome"
+            redirect "/ticket/#{@user.id}"
     end
 
     post '/loginauth' do
@@ -54,78 +54,78 @@ class MainController < Sinatra::Base
         if @user && @user.authenticate(params[:password])
             @user[:sesh_id] = rand(1..100000000000)
             @user.save
-            redirect "/dynamic/#{@user.id}/#{@user.sesh_id}/welcome"
+            redirect "/ticket/#{@user.id}"
         else
             erb :'registration/signup'
         end 
     end
 
-    post '/dynamic/:id/:sesh_id/create_ticket' do
-        @user = Employee.find_by(id: params[:id], sesh_id: params[:sesh_id] )
+    post '/ticket/:id/create_ticket' do
+        @user = Employee.find_by(id: params[:id])
         if params[:title] != "" && params[:details] != ""  
             Ticket.create(title: params[:title], belongs_to: @user.username, details: params[:details], employee_id: @user.id)
-            redirect "/dynamic/#{@user.id}/#{@user.sesh_id}/welcome"
+            redirect "/ticket/#{@user.id}"
         else 
-            redirect "/dynamic/#{@user.id}/#{@user.sesh_id}/create_tickets"
+            redirect "/ticket/#{@user.id}/#{@user.sesh_id}/create_tickets"
         end
     end 
 
-    post '/dynamic/:id/:sesh_id/update_ticket/:ticket_id' do
-        @user = Employee.find_by(id: params[:id], sesh_id: params[:sesh_id] )
+    post '/ticket/:id/update_ticket/:ticket_id' do
+        @user = Employee.find_by(id: params[:id])
         @ticket = Ticket.find_by(id: params[:ticket_id])
         @ticket.title = params[:title]
         @ticket.details = params[:details]
         @ticket.save
-        redirect "/dynamic/#{@user.id}/#{@user.sesh_id}/welcome"
+        redirect "/ticket/#{@user.id}"
     end
 
-    get '/dynamic/:id/:sesh_id/welcome' do
-        @user = Employee.find_by(id: params[:id], sesh_id: params[:sesh_id] )
+    get '/ticket/:id' do
+        @user = Employee.find_by(id: params[:id])
         erb :'employee/main_pg_employees'
     end
 
-    get '/dynamic/:id/:sesh_id/logout' do
-        @user = Employee.find_by(id: params[:id], sesh_id: params[:sesh_id])
+    get '/ticket/:id/logout' do
+        @user = Employee.find_by(id: params[:id])
         @user[:sesh_id] = nil
         @user.save
         redirect '/'
     end 
 
-    get '/dynamic/:id/:sesh_id/all_tickets' do
-        @user = Employee.find_by(id: params[:id], sesh_id: params[:sesh_id])
+    get '/ticket/:id/all_tickets' do
+        @user = Employee.find_by(id: params[:id])
         @tickets = Ticket.all
         erb :'ticket/main_pg_tickets'
     end 
 
-    get '/dynamic/:id/:sesh_id/create_tickets' do
+    get '/ticket/:id/create_tickets' do
         @user = Employee.find_by(id: params[:id])
         erb :'ticket/user_creates_ticket_for_self'
     end
 
-    get '/dynamic/:id/:sesh_id/read_ticket/:ticket_id' do
-        @user = Employee.find_by(id: params[:id], sesh_id: params[:sesh_id])
+    get '/ticket/:id/read_ticket/:ticket_id' do
+        @user = Employee.find_by(id: params[:id])
         @ticket = Ticket.find_by(id: params[:ticket_id])
         erb :'ticket/read_ticket'
     end
 
-    get '/dynamic/:id/:sesh_id/delete_ticket/:ticket_id' do
-        @user = Employee.find_by(id: params[:id], sesh_id: params[:sesh_id])
+    get '/ticket/:id/delete_ticket/:ticket_id' do
+        @user = Employee.find_by(id: params[:id])
         @ticket = Ticket.find_by(id: params[:ticket_id])
         if @user.id == @ticket.employee_id
             Ticket.delete(params[:ticket_id].to_i)
-            redirect "/dynamic/#{@user.id}/#{@user.sesh_id}/welcome"
+            redirect "/ticket/#{@user.id}"
         else
-            redirect "/dynamic/#{@user.id}/#{@user.sesh_id}/welcome"
+            redirect "/ticket/#{@user.id}"
         end
     end
 
-    get '/dynamic/:id/:sesh_id/update_ticket/:ticket_id' do 
-        @user = Employee.find_by(id: params[:id], sesh_id: params[:sesh_id])
+    get '/ticket/:id/update_ticket/:ticket_id' do 
+        @user = Employee.find_by(id: params[:id])
         @ticket = Ticket.find_by(id: params[:ticket_id])
         if @user.id == @ticket.employee_id
             erb :'ticket/update_ticket'
         else
-            redirect "/dynamic/#{@user.id}/#{@user.sesh_id}/welcome"
+            redirect "/ticket/#{@user.id}"
         end
     end 
 
